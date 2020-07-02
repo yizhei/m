87dc941542a0b91400c110c6c7e1bb95f52fee5c
fanxuebing_ext <fanxuebing_ext@cop.cmsoft.com.cn>
2020-07-02 10:39:50 +0800

    first commit

---
+++ tank/src/main/java/com/fan/m/tank/TankApplication.java
@@ -0,0 +1,13 @@
+package com.fan.m.tank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TankApplication.class, args);
+    }
+
+}
---
+++ tank/src/test/java/com/fan/m/tank/TankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fan.m.tank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
